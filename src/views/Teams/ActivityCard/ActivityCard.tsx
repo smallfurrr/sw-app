adc9e9be1c311e087ae45b9620519218f98b2a44
Elise Chia <swedesurrender@gmail.com>
2022-05-24 20:23:20 +0800

    refactor getActivityText function

--- src/views/Teams/ActivityCard/ActivityCard.tsx
+++ src/views/Teams/ActivityCard/ActivityCard.tsx
@@ -10,31 +10,33 @@ const ActivityCard: React.FC<ActivityCardProps> = ({ activity }) => {
   const { person, action, target, created_at } = activity;
   const { avatar, name } = person;
 
+  const lowercaseAction = action.toLowerCase();
+
   const getActivityText = (name: string, action: string, target: string) => {
     if (action.includes('increased')) {
       return (
-        <p className={`mb-0 ${styles['activity-body']}`}>
+        <>
           <span className={`${styles['activity-body-bold']}`}>{name}</span>{' '}
           increased{' '}
           <span className={`${styles['activity-body-bold']}`}>{target}</span>
           &apos;s quota.
-        </p>
+        </>
       );
     } else if (action.includes('added')) {
       return (
-        <p className={`mb-0 ${styles['activity-body']}`}>
+        <>
           <span className={`${styles['activity-body-bold']}`}>{name}</span>{' '}
           added new leads to{' '}
           <span className={`${styles['activity-body-bold']}`}>{target}</span>.
-        </p>
+        </>
       );
     } else if (action.includes('archived')) {
       return (
-        <p className={`mb-0 ${styles['activity-body']}`}>
+        <>
           <span className={`${styles['activity-body-bold']}`}>{name}</span>{' '}
           archived the team{' '}
           <span className={`${styles['activity-body-bold']}`}>{target}</span>.
-        </p>
+        </>
       );
     }
     return '';
@@ -47,7 +49,10 @@ const ActivityCard: React.FC<ActivityCardProps> = ({ activity }) => {
         style={{ backgroundImage: `url(${avatar})` }}
       />
       <div>
-        {getActivityText(name, action, target)}
+        <p className={`mb-0 ${styles['activity-body']}`}>
+          {getActivityText(name, lowercaseAction, target)}
+        </p>
+
         <span className={`${styles['activity-created-text']}`}>
           {created_at}
         </span>
